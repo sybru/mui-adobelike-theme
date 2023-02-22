4a709ce0f316be344050df8a6e2a31f35a8d7705
sybru <kazukiurabayashi@gmail.com>
2023-02-22 17:01:41 +0900

    build

---
+++ dist/0.0.1/theme.d.ts
@@ -0,0 +1,10 @@
+declare module '@mui/material/Button' {
+    interface ButtonPropsVariantOverrides {
+        "adobe-primary-contained": true;
+        "adobe-warning-contained": true;
+        "adobe-primary-outlined": true;
+        "adobe-warning-outlined": true;
+        "adobe-cta": true;
+    }
+}
+export declare const afterEffectsTheme: import("@mui/material/styles").Theme;
