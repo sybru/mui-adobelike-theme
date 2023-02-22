82ee4564877fe9baac0dd1c439c381b9accb53c2
sybru <kazukiurabayashi@gmail.com>
2023-02-22 15:44:00 +0900

    Fix missing "dist"

---
+++ dist/0.0.1/color.d.ts
@@ -0,0 +1,58 @@
+export declare const Color: {
+    AfterEffects: {
+        readonly skin: {
+            readonly common: "#272727";
+            readonly divider: "#1F1F1F";
+            readonly buttonPrimary: "#288CE7";
+            readonly buttonCta: "#272727";
+            readonly buttonCtaHover: "#E8E8E8";
+            readonly buttonWarning: "#F53C3C";
+            readonly toggleButtonEnable: "#161616";
+            readonly dropdown: "#1D1D1D";
+            readonly dropdownBox: "#1D1D1D";
+            readonly list: "#272727";
+            readonly listItem: "#272727";
+            readonly listItemSelected: "#333333";
+            readonly sliderRail: "#454545";
+            readonly sliderThumb: "#272727";
+            readonly sliderThumbActive: "#288CE7";
+            readonly tooltip: "#454545";
+        };
+        readonly text: {
+            readonly common: "#A4A4A4";
+            readonly commonLight: "#C4C4C4";
+            readonly disabled: "#575757";
+            readonly button: "#E8E8E8";
+            readonly buttonPrimary: "#288CE7";
+            readonly buttonWarning: "#F53C3C";
+            readonly buttonCta: "#E8E8E8";
+            readonly buttonCtaHover: "#272727";
+            readonly highlight: "#278CE7";
+            readonly dropdown: "#C5C5C5";
+            readonly warning: "#F53C3C";
+        };
+        readonly border: {
+            readonly buttonPrimary: "#288CE7";
+            readonly buttonWarning: "#F53C3C";
+            readonly buttonCta: "#E8E8E8";
+            readonly toggleButtonEnable: "#313131";
+            readonly disabled: "#575757";
+            readonly dropdown: "#313131";
+            readonly checkbox: "#8A8A8A";
+            readonly sliderThumb: "#8A8A8A";
+            readonly sliderThumbActive: "#288CE7";
+            readonly warning: "#F53C3C";
+        };
+        readonly icon: {
+            readonly common: "#A4A4A4";
+            readonly primary: "#288CE7";
+            readonly warning: "#F53C3C";
+            readonly dropdown: "#B8B8B8";
+            readonly accordion: "#B8B8B8";
+        };
+        readonly hover: {
+            readonly dropdownBox: "#454545";
+            readonly checkbox: "#B9B9B9";
+        };
+    };
+};
